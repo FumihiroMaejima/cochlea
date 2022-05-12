02d5509662ad1871ca1fd87e6909726cbe810e08
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-05-13 02:04:19 +0900

    change extends to BaseSeeder.

--- app/backend/database/seeders/UsersTableSeeder.php
+++ app/backend/database/seeders/UsersTableSeeder.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Config;
 use App\Models\User;
 use Database\Seeders\BaseSeeder;
 
-class UsersTableSeeder extends Seeder
+class UsersTableSeeder extends BaseSeeder
 {
     protected const SEEDER_DATA_LENGTH = 5;
     protected const SEEDER_DATA_TESTING_LENGTH = 5;
@@ -40,6 +40,9 @@ class UsersTableSeeder extends Seeder
         // insert用データ
         $data = [];
 
+        // データ数
+        $this->count = $this->getSeederDataLengthByEnv(Config::get('app.env'));
+
         // 1~$this->countの数字の配列でforを回す
         foreach (range(1, $this->count) as $i) {
             $row = $template;
@@ -52,5 +55,24 @@ class UsersTableSeeder extends Seeder
 
         // テーブルへの格納
         DB::table($this->tableName)->insert($data);
+    }
+
+    /**
+     * get data length by env in parent class pethod.
+     *
+     * @param string $envName 環境の値(local,stg,production,testingなど)
+     * @param int $productionLength production時のインサートするデータ数
+     * @param int $testingLength testing時のインサートするデータ数
+     * @param int $developLength localや開発時のインサートするデータ数
+     * @return int
+     */
+    protected function _getSeederDataLengthByEnv(
+        string $envName,
+        int $productionLength = self::SEEDER_DATA_LENGTH,
+        int $testingLength = self::SEEDER_DATA_TESTING_LENGTH,
+        int $developLength = self::SEEDER_DEVELOP_DATA_LENGTH,
+    ): int
+    {
+        return parent::getSeederDataLengthByEnv($envName, $productionLength, $testingLength, $developLength);
     }
 }
