22cb8a3b0de032ff8fbae0434b2ae22b614aa47a
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-05-27 02:38:19 +0900

    modify permission seeder.

--- app/backend/database/seeders/PermissionsTableSeeder.php
+++ app/backend/database/seeders/PermissionsTableSeeder.php
@@ -6,12 +6,16 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Config;
+use App\Models\Permissions;
+use Database\Seeders\BaseSeeder;
 
-class PermissionsTableSeeder extends Seeder
+class PermissionsTableSeeder extends BaseSeeder
 {
-    private const TABLE_NAME = 'permissions';
-    private const SEEDER_DATA_LENGTH = 5;
-    private int $count = 4;
+    protected const SEEDER_DATA_LENGTH = 4;
+    protected const SEEDER_DATA_TESTING_LENGTH = 4;
+    protected const SEEDER_DEVELOP_DATA_LENGTH = 4;
+    protected int $count = 4;
+    protected string $tableName = '';
 
     /**
      * Run the database seeds.
@@ -20,13 +24,20 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
+        $this->tableName = (new Permissions())->getTable();
+
+        $now = Carbon::now()->timezone(Config::get('app.timeZone'));
+
         $template = [
             'name'       => '',
-            'created_at' => '2021-01-14 00:00:00',
-            'updated_at' => '2021-01-14 00:00:00'
+            'created_at' => $now,
+            'updated_at' => $now
         ];
 
         $dataList = Config::get('myapp.seeder.authority.permissionsNameList');
+
+        // データ数
+        $this->count = $this->_getSeederDataLengthByEnv(Config::get('app.env'));
 
         // insert用データ
         $data = [];
@@ -41,6 +52,25 @@ class PermissionsTableSeeder extends Seeder
         }
 
         // テーブルへの格納
-        DB::table(self::TABLE_NAME)->insert($data);
+        DB::table($this->tableName)->insert($data);
+    }
+
+    /**
+     * get data length by env in parent class pethod.
+     *
+     * @param string $envName 環境の値(local,stg,production,testingなど)
+     * @param int $productionLength production時のインサートするデータ数
+     * @param int $testingLength testing時のインサートするデータ数
+     * @param int $developLength localや開発時のインサートするデータ数
+     * @return int
+     */
+    protected function _getSeederDataLengthByEnv(
+        string $envName,
+        int $productionLength = self::SEEDER_DATA_LENGTH,
+        int $testingLength = self::SEEDER_DATA_TESTING_LENGTH,
+        int $developLength = self::SEEDER_DEVELOP_DATA_LENGTH,
+    ): int
+    {
+        return parent::getSeederDataLengthByEnv($envName, $productionLength, $testingLength, $developLength);
     }
 }
