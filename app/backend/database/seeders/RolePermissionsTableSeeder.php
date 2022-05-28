b23ed3488ad76a7470f1bb39209a15898491fd06
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-05-28 15:40:10 +0900

    use extend method in role permissions.

--- app/backend/database/seeders/RolePermissionsTableSeeder.php
+++ app/backend/database/seeders/RolePermissionsTableSeeder.php
@@ -46,7 +46,12 @@ class RolePermissionsTableSeeder extends BaseSeeder
         $data = [];
 
         // データ数
-        $this->count = $this->_getSeederDataLengthByEnv(Config::get('app.env'));
+        $this->count = $this->getSeederDataLengthByEnv(
+            Config::get('app.env'),
+            self::SEEDER_DATA_LENGTH,
+            self::SEEDER_DATA_TESTING_LENGTH,
+            self::SEEDER_DEVELOP_DATA_LENGTH
+        );
 
         // 1~$this->countの数字の配列でforを回す
         foreach (range(1, $this->count) as $i) {
@@ -93,24 +98,5 @@ class RolePermissionsTableSeeder extends BaseSeeder
             'created_at'    => $dateTime,
             'updated_at'    => $dateTime
         ];
-    }
-
-        /**
-     * get data length by env in parent class pethod.
-     *
-     * @param string $envName 環境の値(local,stg,production,testingなど)
-     * @param int $productionLength production時のインサートするデータ数
-     * @param int $testingLength testing時のインサートするデータ数
-     * @param int $developLength localや開発時のインサートするデータ数
-     * @return int
-     */
-    protected function _getSeederDataLengthByEnv(
-        string $envName,
-        int $productionLength = self::SEEDER_DATA_LENGTH,
-        int $testingLength = self::SEEDER_DATA_TESTING_LENGTH,
-        int $developLength = self::SEEDER_DEVELOP_DATA_LENGTH,
-    ): int
-    {
-        return parent::getSeederDataLengthByEnv($envName, $productionLength, $testingLength, $developLength);
     }
 }
