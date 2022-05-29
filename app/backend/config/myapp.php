53cf65f81aeba3609812ebd56b5be850749498ca
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-05-30 01:03:29 +0900

    modify auth admin login route name.

--- app/backend/config/myapp.php
+++ app/backend/config/myapp.php
@@ -26,7 +26,35 @@ return [
             ]
         ]
     ],
-    'test' => [],
+    'test' => [
+        'admin' => [
+            'login' => [
+                'email'    => 'test' . 'admin1' . '@example.com',
+                'password' => env('TEST_ADMIN_SEEDER_PASSWORD', 'password')
+            ]
+        ],
+        'member' => [
+            'create' => [
+                'success' => [
+                    'name'                  => 'test name',
+                    'email'                 => 'testadmin'. '12345XXX' . '@example.com',
+                    'roleId'                => 1,
+                    'password'              => 'testpassword' . '12345',
+                    'password_confirmation' => 'testpassword' . '12345'
+                ]
+            ]
+        ],
+        'roles' => [
+            'create' => [
+                'success' => [
+                    'name'        => 'test name',
+                    'code'        => 'test_code',
+                    'detail'      => 'role`s detail.',
+                    'permissions' => [1,2,3],
+                ]
+            ]
+        ]
+    ],
     'headers' => [
         'id'        => 'X-Auth-ID',
         'authority' => 'X-Auth-Authority'
