9251c1544528582a650d1bf0cbc4c4b8ec53f418
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-06-02 02:37:17 +0900

    modify admins service.

--- app/backend/app/Services/AdminsService.php
+++ app/backend/app/Services/AdminsService.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\Admins\AdminsExport;
-use App\Repositories\AdminsRoles\AdminsRolesRepositoryInterface;
+use App\Repositories\Admins\AdminsRoles\AdminsRolesRepositoryInterface;
 use App\Repositories\Admins\AdminsRepositoryInterface;
 use App\Http\Requests\Admins\AdminCreateRequest;
 use App\Http\Requests\Admins\AdminDeleteRequest;
@@ -19,6 +19,9 @@ use App\Http\Requests\Admins\AdminUpdateRequest;
 use App\Http\Resources\Admins\AdminCreateResource;
 use App\Http\Resources\Admins\AdminDeleteResource;
 use App\Http\Resources\Admins\AdminsCollection;
+use App\Http\Resources\Admins\AdminsRolesCreateResource;
+use App\Http\Resources\Admins\AdminsRolesDeleteResource;
+use App\Http\Resources\Admins\AdminsRolesUpdateResource;
 use App\Http\Resources\Admins\AdminsResource;
 use App\Http\Resources\Admins\AdminUpdateResource;
 use App\Http\Resources\Admins\AdminUpdateNotificationResource;
@@ -30,18 +33,18 @@ use Exception;
 class AdminsService
 {
     protected $adminsRepository;
-    // protected $adminsRolesRepository;
+    protected $adminsRolesRepository;
 
     /**
      * create AdminsService instance
      * @param \App\Repositories\Admins\AdminsRepositoryInterface $adminsRepository
-     * @param \App\Repositories\AdminsRoles\AdminsRolesRepositoryInterface $adminsRepository
+     * @param \App\Repositories\Admins\AdminsRoles\AdminsRolesRepositoryInterface $adminsRepository
      * @return void
      */
-    public function __construct(AdminsRepositoryInterface $adminsRepository)
+    public function __construct(AdminsRepositoryInterface $adminsRepository, AdminsRolesRepositoryInterface $adminsRolesRepository)
     {
         $this->adminsRepository = $adminsRepository;
-        // $this->adminsRolesRepository = $adminsRolesRepository;
+        $this->adminsRolesRepository = $adminsRolesRepository;
     }
 
     /**
@@ -94,14 +97,14 @@ class AdminsService
             $latestAdmin = $this->adminsRepository->getLatestAdmin();
 
             // 権限情報の作成
-            // $adminsRolesResource = app()->make(AdminsRolesCreateResource::class, ['resource' => $latestAdmin])->toArray($request);
-            // $insertAdminsRolesCount = $this->adminsRolesRepository->createAdminsRole($adminsRolesResource);
+            $adminsRolesResource = app()->make(AdminsRolesCreateResource::class, ['resource' => $latestAdmin])->toArray($request);
+            $insertAdminsRolesCount = $this->adminsRolesRepository->createAdminsRole($adminsRolesResource);
 
             DB::commit();
 
             // 作成されている場合は304
-            $message = ($insertCount > 0) ? 'success' : 'Bad Request';
-            $status = ($insertCount > 0) ? 201 : 401;
+            $message = ($insertCount > 0 && $insertAdminsRolesCount > 0) ? 'success' : 'Bad Request';
+            $status = ($insertCount > 0 && $insertAdminsRolesCount > 0) ? 201 : 401;
 
             return response()->json(['message' => $message, 'status' => $status], $status);
         } catch (Exception $e) {
@@ -127,8 +130,8 @@ class AdminsService
             $updatedRowCount = $this->adminsRepository->updateAdminData($resource, $id);
 
             // 権限情報の更新
-            // $roleIdResource = app()->make(AdminsRolesUpdateResource::class, ['resource' => $request])->toArray($request);
-            // $updatedAdminsRolesRowCount = $this->adminsRolesRepository->updateAdminsRoleData($roleIdResource, $id);
+            $roleIdResource = app()->make(AdminsRolesUpdateResource::class, ['resource' => $request])->toArray($request);
+            $updatedAdminsRolesRowCount = $this->adminsRolesRepository->updateAdminsRoleData($roleIdResource, $id);
 
             // slack通知
             $attachmentResource = app()->make(AdminUpdateNotificationResource::class, ['resource' => ":tada: Update Member Data \n"])->toArray($request);
@@ -137,8 +140,8 @@ class AdminsService
             DB::commit();
 
             // 更新されていない場合は304
-            $message = ($updatedRowCount > 0) ? 'success' : 'not modified';
-            $status = ($updatedRowCount > 0) ? 200 : 304;
+            $message = ($updatedRowCount > 0 || $updatedAdminsRolesRowCount > 0) ? 'success' : 'not modified';
+            $status = ($updatedRowCount > 0 || $updatedAdminsRolesRowCount > 0) ? 200 : 304;
 
             return response()->json(['message' => $message, 'status' => $status], $status);
         } catch (Exception $e) {
@@ -166,14 +169,14 @@ class AdminsService
             $deleteRowCount = $this->adminsRepository->deleteAdminData($resource, $request->id);
 
             // 権限情報の更新
-            // $roleIdResource = app()->make(AdminsRolesDeleteResource::class, ['resource' => $request])->toArray($request);
-            // $deleteAdminsRolesRowCount = $this->adminsRolesRepository->deleteAdminsRoleData($roleIdResource, $id);
+            $roleIdResource = app()->make(AdminsRolesDeleteResource::class, ['resource' => $request])->toArray($request);
+            $deleteAdminsRolesRowCount = $this->adminsRolesRepository->deleteAdminsRoleData($roleIdResource, $id);
 
             DB::commit();
 
             // 更新されていない場合は304
-            $message = ($deleteRowCount > 0) ? 'success' : 'not deleted';
-            $status = ($deleteRowCount > 0) ? 200 : 401;
+            $message = ($deleteRowCount > 0 && $deleteAdminsRolesRowCount > 0) ? 'success' : 'not deleted';
+            $status = ($deleteRowCount > 0 && $deleteAdminsRolesRowCount > 0) ? 200 : 401;
 
             return response()->json(['message' => $message, 'status' => $status], $status);
         } catch (Exception $e) {
