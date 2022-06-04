3d6eddbf78e5bc714ea76e6d802c90ca183fbd14
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-06-04 23:22:06 +0900

    add getCurrentDateTime to fromat parameter.

--- app/backend/app/Library/TimeLibrary.php
+++ app/backend/app/Library/TimeLibrary.php
@@ -10,20 +10,27 @@ use Illuminate\Support\Carbon;
 class TimeLibrary
 {
     // デフォルトのフォーマット
-    private const DEFAULT_DATE_TIME_FORMAT = 'Y-m-d H:i:s';
+    public const DEFAULT_DATE_TIME_FORMAT = 'Y-m-d H:i:s'; // ex: 2022-01-01 00:00:00
+    public const DEFAULT_DATE_TIME_FORMAT_SLASH = 'Y/m/d H:i:s'; // ex: 2022/01/01 00:00:00
+
+    public const DATE_TIME_FORMAT_YMD = 'Ymd'; // ex: 20220101
+    public const DATE_TIME_FORMAT_HIS = 'His'; // ex: 000000
+    public const DATE_TIME_FORMAT_YMDHIS = 'YmdHis'; // ex: 20220101125959
 
     /**
      * get current date time.
      *
+     * @param string $format datetime format
      * @return string
      */
-    public static function getCurrentDateTime(): string
+    public static function getCurrentDateTime(string $format = self::DEFAULT_DATE_TIME_FORMAT): string
     {
         /* $carbon = new Carbon();
         $test = $carbon->now()->format('Y-m-d H:i:s'); */
         // $dateTime = Carbon::now()->format('Y-m-d H:i:s');
 
-        return Carbon::now()->format(self::DEFAULT_DATE_TIME_FORMAT);
+        // return Carbon::now()->format(self::DEFAULT_DATE_TIME_FORMAT);
+        return Carbon::now()->timezone(Config::get('app.timezone'))->format($format);
     }
 
     /**
