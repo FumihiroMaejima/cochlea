2d63af372436c4a2656cfdfa6ec675ccbe4c8d4e
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-06-11 18:00:49 +0900

    modify test class .

--- app/backend/tests/Unit/Service/AdminsServiceTest.php
+++ app/backend/tests/Unit/Service/AdminsServiceTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Service;
 // use PHPUnit\Framework\TestCase;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\WithFaker;
 // use Illuminate\Foundation\Testing\DatabaseMigrations;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -35,14 +36,18 @@ class AdminsServiceTest extends TestCase
         // $this->refreshTestDatabase();
         // $this->runDatabaseMigrations();
 
-        $this->artisan('migrate:fresh');
+        Artisan::call('migrate:fresh');
+        Artisan::call('db:seed');
+
+        // TODO テスト後に戻す
+        /* $this->artisan('migrate:fresh');
         $this->seed([
             AdminsTableSeeder::class,
             PermissionsTableSeeder::class,
             RolesTableSeeder::class,
             RolePermissionsTableSeeder::class,
             AdminsRolesTableSeeder::class,
-        ]);
+        ]); */
 
         $response = $this->json('POST', route('auth.admin.login'), [
             'email'    => Config::get('myapp.test.admin.login.email'),
