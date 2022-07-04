19ee825a31c53284b6846477e4742bf0bf7a176c
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-07-05 01:37:21 +0900

    modify get request.

--- app/backend/app/Library/Stripe/StripeLibrary.php
+++ app/backend/app/Library/Stripe/StripeLibrary.php
@@ -21,7 +21,7 @@ class StripeLibrary
     private const REQUEST_METHOD_DELETE = 'DELETE';
 
     // api key config name
-    private const CONFIG_KEY_NAME = 'stripe.apiKey.private';
+    private const CONFIG_KEY_NAME_PRIVATE = 'stripe.apiKey.private';
 
     /**
      * get current date time.
@@ -44,41 +44,42 @@ class StripeLibrary
     {
         $stripe = self::getStripeClient();
         // $stripe->getApiBase();
-        // $stripe->request(self::REQUEST_METHOD_GET, '/');
+        // $stripe->request('GET', '/');
+
         // TODO 確認したいものからコメントアウトを外す。
         $resource = [
             'getApiBase' => $stripe->getApiBase(),
             // 'customers' => $stripe->customers,
-            // 'api_customers' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/customers', [], []),
-            //'api_customers' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/customers', ['limit' => 3], []),
-            // 'api_balance' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/balance', [], []),
-            // 'api_balance_transactions' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/balance_transactions', [], []),
-            // 'api_charges' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/charges', [], []),
-            // 'api_disputes' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/disputes', [], []),
-            // 'api_events' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/events', [], []),
-            // 'api_files' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/files', [], []),
-            // 'api_file_links' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/file_links', [], []),
-            // 'api_payment_intents' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/payment_intents', [], []),
-            // 'api_setup_intents' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/setup_intents', [], []),
-            // 'api_setup_attempts' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/setup_attempts', [], []),
-            // 'api_payouts' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/payouts', [], []),
-            // 'api_payment_methods' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/payment_methods', [], []),
-            // 'api_products' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/products', [], []),
-            // 'api_prices' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/prices', [], []),
-            // 'api_coupons' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/coupons', [], []),
-            // 'api_promotion_codes' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/promotion_codes', [], []),
-            // 'api_tax_codes' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/tax_codes', [], []),
-            // 'api_invoices' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/invoices', [], []),
-            // 'api_invoiceitems' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/invoiceitems', [], []),
-            // 'api_subscriptions' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/subscriptions', [], []),
-            'api_orders' => $stripe->request(self::REQUEST_METHOD_GET, '/v1/orders', [], []),
+            // 'api_customers' => self::getRequest('/v1/customers'),
+            //'api_customers' => self::getRequest('/v1/customers', ['limit' => 3], []),
+            // 'api_balance' => self::getRequest('/v1/balance'),
+            // 'api_balance_transactions' => self::getRequest('/v1/balance_transactions'),
+            // 'api_charges' => self::getRequest('/v1/charges'),
+            // 'api_disputes' => self::getRequest('/v1/disputes'),
+            // 'api_events' => self::getRequest('/v1/events'),
+            // 'api_files' => self::getRequest('/v1/files'),
+            // 'api_file_links' => self::getRequest('/v1/file_links'),
+            // 'api_payment_intents' => self::getRequest('/v1/payment_intents'),
+            // 'api_setup_intents' => self::getRequest('/v1/setup_intents'),
+            // 'api_setup_attempts' => self::getRequest('/v1/setup_attempts'),
+            // 'api_payouts' => self::getRequest('/v1/payouts'),
+            // 'api_payment_methods' => self::getRequest('/v1/payment_methods'),
+            // 'api_products' => self::getRequest('/v1/products'),
+            // 'api_prices' => self::getRequest('/v1/prices'),
+            // 'api_coupons' => self::getRequest('/v1/coupons'),
+            // 'api_promotion_codes' => self::getRequest('/v1/promotion_codes'),
+            // 'api_tax_codes' => self::getRequest('/v1/tax_codes'),
+            // 'api_invoices' => self::getRequest('/v1/invoices'),
+            // 'api_invoiceitems' => self::getRequest('/v1/invoiceitems'),
+            // 'api_subscriptions' => self::getRequest('/v1/subscriptions'),
+            'api_orders' => self::getRequest('/v1/orders'),
         ];
 
         return response()->json($resource, 200);
     }
 
     /**
-     * get permissions data for frontend parts
+     * exec stripe api request for GET
      *
      * @param string $path — the path of the request
      * @param array $params — the parameters of the request
@@ -103,6 +104,6 @@ class StripeLibrary
      */
     private static function getStripeClient(): StripeClient
     {
-        return new StripeClient(Config::get(self::CONFIG_KEY_NAME));
+        return new StripeClient(Config::get(self::CONFIG_KEY_NAME_PRIVATE));
     }
 }
