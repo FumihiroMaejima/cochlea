e66d01cb62d1d754157e8d1479b15ba7a05449f0
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-07-07 01:04:15 +0900

    create coins request files.

---
+++ app/backend/app/Http/Requests/Admins/Coins/CoinUpdateRequest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Requests\Admins\Coins;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use App\Http\Requests\BaseRequest;
+use App\Library\TimeLibrary;
+
+// use Symfony\Component\HttpKernel\Exception\HttpException;
+// use Illuminate\Validation\ValidationException;
+
+class CoinUpdateRequest extends BaseRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $this->requestAuthorities = Config::get('myapp.executionRole.services.coins');
+        return parent::authorize();
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        // ルーティングで設定しているidパラメーターをリクエストデータとして設定する
+        $this->merge(['id' => $this->route('id')]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id'          => 'required|integer|min:4',  // master~developmentは更新不可
+            'name'        => 'required|string|between:1,50',
+            'detail'      => 'required|string|between:1,100',
+            'price'       => 'required|integer|min:0|max:100000',
+            'cost'        => 'required|integer|min:0|max:100000',
+            'start_at'    => 'required|date|date_format:'.TimeLibrary::DEFAULT_DATE_TIME_FORMAT,
+            'end_at'      => 'required|date|date_format:'.TimeLibrary::DEFAULT_DATE_TIME_FORMAT,
+            'image'       => 'file|image|max:512', // 最大512KB
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'id.integer' => ':attributeは整数で入力してください。',
+            'required'    => ':attributeは必須項目です。',
+            'integer'     => ':attributeは整数で入力してください。',
+            'string'      => ':attributeは文字列を入力してください。',
+            'array'       => ':attributeは配列で入力してください。',
+            'between'     => ':attributeは:min〜:max文字以内で入力してください。',
+            'min'         => ':attributeは:min以上で入力してください。',
+            'max'         => ':attributeは:max以下で入力してください。',
+            'date'        => ':attributeは日付の形式で入力してください。',
+            'date_format' => ':attributeは:date_formatの形式で入力してください。',
+            'file'        => ':attributeはファイル形式で入力してください。',
+            'image'       => ':attributeは画像ファイルで入力してください。',
+            'image.max'   => ':attributeは最大:max KBで入力してください。',
+            'exists'      => '指定した:attributeは存在しません。'
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'id'       => 'id',
+            'name'     => 'コイン名',
+            'detail'   => '詳細',
+            'price'    => 'コインの購入価格',
+            'cost'     => 'アプリケーション内のコインの価格',
+            'start_at' => '公開開始日時',
+            'end_at'   => '公開終了日時',
+            'image'    => 'イメージ',
+        ];
+    }
+}
