dc5c1e1b7a6839cba5c5e5959fcab0b6d2647966
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-07-12 01:01:12 +0900

    cread wieghted random value.

---
+++ app/backend/app/Library/Random/RandomLibrary.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Library\Random;
+
+use Exception;
+use App\Exceptions\ExceptionStatusCodeMessages;
+
+class RandomLibrary
+{
+    private const RANDOM_MIN_VALUE = 1;
+
+    /**
+     * 重み付き抽選関数
+     *
+     * @param array<string|int, int> $entries ($key => $weight)
+     * @return int random value
+     */
+    public static function getWeightedRandomValue(array $entries): int
+    {
+        // 配列の値($weight)を全て足す。
+        // objectやcollectionから算出する場合はarray_map()など別配列化する必要がある。
+        $sum  = array_sum($entries);
+        $rand = rand(self::RANDOM_MIN_VALUE, $sum);
+
+        foreach($entries as $key => $weight){
+            // ($a = $a - $b)と同様(減算)
+            if (($sum -= $weight) < $rand) return $key;
+        }
+
+        throw new Exception('failed getting random value,', ExceptionStatusCodeMessages::STATUS_CODE_500);
+    }
+}
