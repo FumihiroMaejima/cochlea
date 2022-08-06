3cd515c0b2948536eb4e846e566261fa2c4e58d5
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-08-06 16:09:01 +0900

    modify create paritionsetting.

--- app/backend/app/Console/Commands/Admins/Database/CheckLogDatabasePartitionCommand.php
+++ app/backend/app/Console/Commands/Admins/Database/CheckLogDatabasePartitionCommand.php
@@ -237,8 +237,8 @@ class CheckLogDatabasePartitionCommand extends Command
 
         // 追加する分のパーティション設定を作成
         foreach (range(0, $count) as $i) {
-            $target = ($i * $baseNumber) + $id;
-            $next = $baseNumber * ($i + $id);
+            $target = $i !== 0 ? (($i * $baseNumber) + $id) : $id;
+            $next = ($target + $baseNumber - 1);
 
             $partitionSetting = "PARTITION p${target} VALUES LESS THAN (${next})" . ($i <= ($count - 1) ? ', ' : '');
             $partitions .= $partitionSetting;
