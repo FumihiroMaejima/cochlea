c55084cb094b2ec3db96d41be40131c77fe8f4c7
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-08-17 01:32:50 +0900

    create .env.testing settiing.

--- mysql/sql/initialize.sql
+++ mysql/sql/initialize.sql
@@ -12,28 +12,30 @@ FLUSH PRIVILEGES;
 
 -- logs DB
 CREATE DATABASE IF NOT EXISTS cochlea_logs CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON cochlea_logs.* TO 'root'@'%';
 
 FLUSH PRIVILEGES;
 
 -- user DB1
 CREATE DATABASE IF NOT EXISTS cochlea_user1 CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON cochlea_user1.* TO 'root'@'%';
 
 FLUSH PRIVILEGES;
 
 -- user DB2
 CREATE DATABASE IF NOT EXISTS cochlea_user2 CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON cochlea_user2.* TO 'root'@'%';
 
 FLUSH PRIVILEGES;
 
 -- user DB3
 CREATE DATABASE IF NOT EXISTS cochlea_user3 CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON cochlea_user3.* TO 'root'@'%';
+
+FLUSH PRIVILEGES;
+
+-- testing DB
+CREATE DATABASE IF NOT EXISTS cochlea_testing CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+GRANT ALL PRIVILEGES ON cochlea_testing.* TO 'root'@'%';
 
 FLUSH PRIVILEGES;
