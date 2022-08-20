ae818594bd17d7acae670799d3ca5ce1ff91c35d
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-08-20 22:22:14 +0900

    create UuidLibraryTest.

--- app/backend/README.md
+++ app/backend/README.md
@@ -280,6 +280,7 @@ vendor/bin/phpmd . text ruleset.xml --suffixes php --exclude node_modules,resour
 ```shell-session
 vendor/bin/phpunit tests/Unit/ExampleTest --testdox
 vendor/bin/phpunit tests/Unit/Library/TimeLibraryTest.php --testdox
+vendor/bin/phpunit tests/Unit/Library/String/UuidLibraryTest.php --testdox
 ```
 
 カバレッジ出力
---
+++ app/backend/tests/Unit/Library/String/UuidLibraryTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Unit\Service\String;
+
+// use PHPUnit\Framework\TestCase;
+
+use App\Library\Time\TimeLibrary;
+use App\Library\String\UuidLibrary;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Foundation\Testing\WithFaker;
+use DateTime;
+
+// use Illuminate\Foundation\Testing\DatabaseMigrations;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UuidLibraryTest extends TestCase
+{
+    private const VERSION4_STRING = 4;
+    private const HEX_11 = 0b1011;
+
+    /**
+     * setUpは各テストメソッドが実行される前に実行する
+     * 親クラスのsetUpを必ず実行する
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * test uuid version 4.
+     *
+     * @return void
+     */
+    public function testUuidVersion4(): void
+    {
+        $result = UuidLibrary::uuidVersion4();
+
+        // xの位置
+        // strpos(UuidLibrary::PATTERN_V4, UuidLibrary::CHAR_BIT);
+        // yの位置
+        // strpos(UuidLibrary::PATTERN_V4, UuidLibrary::CHAR_VARIANT);
+
+        $this->assertEquals(
+            self::VERSION4_STRING,
+            mb_substr($result, strpos(UuidLibrary::PATTERN_V4, self::VERSION4_STRING), 1)
+        );
+
+        $this->assertGreaterThan(
+            (UuidLibrary::RANDOM_INT_MIN_8 - 1),
+            mb_substr($result, strpos(UuidLibrary::PATTERN_V4, UuidLibrary::CHAR_VARIANT), 1)
+        );
+
+        $this->assertLessThan(
+            dechex(UuidLibrary::RANDOM_INT_MAX_11 + 1),
+            mb_substr($result, strpos(UuidLibrary::PATTERN_V4, UuidLibrary::CHAR_VARIANT), 1)
+        );
+    }
+}
