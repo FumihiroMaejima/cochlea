b08db33b5a84c8743d79952b1412f0793374e76b
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-10-12 01:25:49 +0900

    create TrancateTables console.

---
+++ app/backend/app/Console/Commands/Testing/Database/TrancateTables.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Console\Commands\Testing\Database;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Console\Command;
+use App\Exceptions\MyApplicationHttpException;
+use App\Library\Message\StatusCodeMessages;
+use App\Library\Time\TimeLibrary;
+
+class TrancateTables extends Command
+{
+    /** @var string CONNECTION_NAME_FOR_CI CIなどで使う場合のコネクション名。単一のコネクションに接続させる。 */
+    private const CONNECTION_NAME_FOR_CI = 'sqlite';
+    /** @var string CONNECTION_NAME_FOR_TESTING UnitTestで使う場合のコネクション名。単一のコネクションに接続させる。 */
+    private const CONNECTION_NAME_FOR_TESTING = 'mysql_testing';
+
+    /**
+     * The name and signature of the console command.(コンソールコマンドの名前と使い方)
+     *
+     * @var string
+     */
+    protected $signature = 'testing:truncate {tables*}';
+
+    /**
+     * The console command description.(コンソールコマンドの説明)
+     *
+     * @var string
+     */
+    protected $description = 'debug test command with parameter.';
+
+
+    /**
+     * DebugTestCommandインスタンスの生成
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.(コマンドの実行)
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $values = $this->arguments();
+
+        // 現在日時(タイムゾーン付き)
+        echo date('c') . "\n";
+        echo 'Truncate Tables.' . "\n";
+        // echo var_dump($values) . "\n";
+        if (config('app.env') === 'testing') {
+            self::truncateTable($values);
+        }
+    }
+
+    /**
+     * truncate some tables.
+     *
+     * @param array $tables table name
+     * @param int $mounthCount add partition count as month
+     * @return void
+     */
+    private static function truncateTable(array $tables): void
+    {
+        // 対象のDBの設定
+        $logsConnectionName = Config::get('myapp.database.logs.baseConnectionName');
+        $userConnectionName = Config::get('myapp.database.users.baseConnectionName');
+        $connection = '';
+
+        // connection 設定がCI用の設定の場合
+        if (($logsConnectionName === self::CONNECTION_NAME_FOR_CI) && ($userConnectionName === self::CONNECTION_NAME_FOR_CI)) {
+            $connection = self::CONNECTION_NAME_FOR_CI;
+        } else {
+            // テスト用DB内のテーブル
+            $connection = self::CONNECTION_NAME_FOR_TESTING;
+        }
+        $database = Config::get("database.connections.${connection}.database");
+
+        // TRUNCATEの実行
+        foreach ($tables as $table) {
+            // IF EXISTS
+            DB::statement(
+                "
+                    TRUNCATE TABLE ${database}.${table};
+                "
+            );
+        }
+    }
+}
