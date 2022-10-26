7235ce365bb9ea81dbd2922fc4f9906ffcecd5c5
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-10-26 23:58:30 +0900

    add createUserReadInformation  method.

--- app/backend/app/Http/Controllers/Users/InformationsController.php
+++ app/backend/app/Http/Controllers/Users/InformationsController.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
+use App\Exceptions\MyApplicationHttpException;
+use App\Library\Message\StatusCodeMessages;
 use App\Http\Controllers\Controller;
 use App\Services\Users\InformationsService;
 use App\Trait\CheckHeaderTrait;
@@ -38,5 +41,35 @@ class InformationsController extends Controller
     {
         // サービスの実行
         return $this->service->getInformations();
+    }
+
+    /**
+     * create user read information request handling.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return JsonResponse
+     */
+    public function createUserReadInformation(Request $request): JsonResponse
+    {
+        // バリデーションチェック
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'informationId' => ['required','integer'],
+            ]
+        );
+
+        if ($validator->fails()) {
+            // $validator->errors()->toArray();
+            throw new MyApplicationHttpException(
+                StatusCodeMessages::STATUS_422,
+            );
+        }
+
+        // ユーザーIDの取得
+        $userId = $this->getUserId($request);
+
+        // サービスの実行
+        return $this->service->createUserReadInformation($userId, $request->informationId);
     }
 }
