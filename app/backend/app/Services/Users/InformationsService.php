b07380751171258f773650a412f1cb3cdf5d3b92
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-10-30 19:23:29 +0900

    add echo to controller.

--- app/backend/app/Services/Users/InformationsService.php
+++ app/backend/app/Services/Users/InformationsService.php
@@ -120,14 +120,7 @@ class InformationsService
         } catch (Exception $e) {
             Log::error(__CLASS__ . '::' . __FUNCTION__ . ' line:' . __LINE__ . ' ' . 'message: ' . json_encode($e->getMessage()));
             DB::rollback();
-
-            // TODO 検証用
-            throw new MyApplicationHttpException(
-                StatusCodeMessages::STATUS_500,
-                $e->getMessage(),
-                isResponseMessage: true
-            );
-            // throw $e;
+            throw $e;
         }
 
         return response()->json(
