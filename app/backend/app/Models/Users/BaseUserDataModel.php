9f4dd9d2257f95d3b6776bdbce1561f5fd446ae2
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-11-06 02:07:09 +0900

    remove unused.

--- app/backend/app/Models/Users/BaseUserDataModel.php
+++ app/backend/app/Models/Users/BaseUserDataModel.php
@@ -8,15 +8,6 @@ use App\Library\Database\ShardingLibrary;
 
 class BaseUserDataModel extends Model
 {
-    /** @var array<int, int> NODE1_SHARDS Node1のDBに設定されているシャードのidを格納 */
-    private const NODE1_SHARDS = [1, 4, 7, 10];
-
-    /** @var array<int, int> NODE2_SHARDS Node2のDBに設定されているシャードのidを格納 */
-    private const NODE2_SHARDS = [2, 5, 8, 11];
-
-    /** @var array<int, int> NODE3_SHARDS Node3のDBに設定されているシャードのidを格納 */
-    private const NODE3_SHARDS = [3, 6, 9, 12];
-
     /**
      * get connection name by user id.
      *
@@ -52,31 +43,4 @@ class BaseUserDataModel extends Model
         // 除算の余り
         return ShardingLibrary::getShardIdByUserId($userId);
     }
-
-    /**
-     * get connection name by shard id.
-     *
-     * @param int $shardId shard id.
-     * @return int node name
-     */
-    /* public static function getDataBaseConnection(int $shardId): string
-    {
-        $baseConnectionName = Config::get('myapp.database.users.baseConnectionName');
-
-        if ($baseConnectionName === Config::get('myapp.ci.database.baseConnectionName')) {
-            return $baseConnectionName;
-        }
-
-        // 3で割り切れる場合はnode3
-        if (($shardId % Config::get('myapp.database.users.modBaseNumber')) === 0) {
-            // user database3
-            return $baseConnectionName .(string)Config::get('myapp.database.users.nodeNumber3');
-        } elseif (in_array($shardId, Config::get('myapp.database.users.node1ShardIds'), true)) {
-            // user database1
-            return $baseConnectionName .(string)Config::get('myapp.database.users.nodeNumber1');
-        } else {
-            // user database2
-            return $baseConnectionName .(string)Config::get('myapp.database.users.nodeNumber2');
-        }
-    } */
 }
