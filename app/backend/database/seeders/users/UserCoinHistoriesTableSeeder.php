0cf592c0e5cbb52f72348c5b4f006caf1221f05d
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-11-17 01:13:19 +0900

    add user coin setting.

--- app/backend/database/seeders/users/UserCoinHistoriesTableSeeder.php
+++ app/backend/database/seeders/users/UserCoinHistoriesTableSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Config;
 use App\Exceptions\MyApplicationHttpException;
+use App\Http\Resources\Users\UserCoinsResource;
+use App\Library\Array\ArrayLibrary;
 use App\Library\Message\StatusCodeMessages;
 use App\Library\Database\ShardingLibrary;
 use App\Library\Random\RandomStringLibrary;
@@ -90,7 +92,7 @@ class UserCoinHistoriesTableSeeder extends BaseSeeder
                 case UserCoinHistories::USER_COINS_HISTORY_TYPE_GAIN: // 獲得(無料、有料、期間限定含む)
                     if ($coinType === Coins::COIN_TYPE_FREE) {
                         $row[UserCoinHistories::GET_FREE_COINS] = $price;
-                    } else if ($coinType === Coins::COIN_TYPE_PAID) {
+                    } elseif ($coinType === Coins::COIN_TYPE_PAID) {
                         $row[UserCoinHistories::GET_PAID_COINS] = $price;
                     } else {
                         $row[UserCoinHistories::GET_LIMITED_TIME_COINS] = $price;
@@ -101,7 +103,7 @@ class UserCoinHistoriesTableSeeder extends BaseSeeder
                 case UserCoinHistories::USER_COINS_HISTORY_TYPE_CONSUME: // 消費
                     if ($coinType === Coins::COIN_TYPE_FREE) {
                         $row[UserCoinHistories::USED_FREE_COINS] = $price;
-                    } else if ($coinType === Coins::COIN_TYPE_PAID) {
+                    } elseif ($coinType === Coins::COIN_TYPE_PAID) {
                         $row[UserCoinHistories::USED_PAID_COINS] = $price;
                     } else {
                         $row[UserCoinHistories::USED_LIMITED_TIME_COINS] = $price;
@@ -116,7 +118,7 @@ class UserCoinHistoriesTableSeeder extends BaseSeeder
                 case UserCoinHistories::USER_COINS_HISTORY_TYPE_COMPENSATION: // 補填
                     if ($coinType === Coins::COIN_TYPE_FREE) {
                         $row[UserCoinHistories::GET_FREE_COINS] = $price;
-                    } else if ($coinType === Coins::COIN_TYPE_PAID) {
+                    } elseif ($coinType === Coins::COIN_TYPE_PAID) {
                         $row[UserCoinHistories::GET_PAID_COINS] = $price;
                     } else {
                         $row[UserCoinHistories::GET_LIMITED_TIME_COINS] = $price;
@@ -139,9 +141,53 @@ class UserCoinHistoriesTableSeeder extends BaseSeeder
         }
 
         $model = (new UserCoinHistories());
+        $useCoinModel = (new UserCoins());
         DB::beginTransaction();
         try {
             foreach ($data as $row) {
+                $userId = $row[UserCoinHistories::USER_ID];
+                // ユーザーの所持しているコインの更新
+                $userCoin = $this->getUserCoinByUserId($row[UserCoinHistories::USER_ID]);
+
+                if (is_null($userCoin)) {
+                    // 登録されていない場合は新規登録
+                    $userCoinResource = UserCoinsResource::toArrayForCreate(
+                        $userId,
+                        0,
+                        0,
+                        0
+                    );
+                    $useCoinModel->getQueryBuilder($userId)->insert($userCoinResource);
+                }
+
+                // ロックをかけて再取得
+                $userCoin = $this->getUserCoinByUserId($userId);
+
+                $freeCoin = $userCoin[UserCoins::FREE_COINS] + $row[UserCoinHistories::GET_FREE_COINS] +  $row[UserCoinHistories::USED_FREE_COINS];
+                $paidCoin = $userCoin[UserCoins::PAID_COINS] + $row[UserCoinHistories::GET_PAID_COINS] +  $row[UserCoinHistories::USED_PAID_COINS];
+
+                // ランダムで設定した期限切れコイン数が保有コイン数を超過する場合
+                if ($userCoin[UserCoins::LIMITED_TIME_COINS] < $row[UserCoinHistories::EXPIRED_LIMITED_TIME_COINS]) {
+                    $limitedCoin = 0;
+                } else {
+                    $limitedCoin = $userCoin[UserCoins::LIMITED_TIME_COINS]
+                        + $row[UserCoinHistories::GET_LIMITED_TIME_COINS]
+                        + $row[UserCoinHistories::USED_LIMITED_TIME_COINS]
+                        - $row[UserCoinHistories::EXPIRED_LIMITED_TIME_COINS];
+                }
+
+                // ユーザーのコイン情報の更新
+                $userCoinResource = UserCoinsResource::toArrayForUpdate(
+                    $userId,
+                    $freeCoin,
+                    $paidCoin,
+                    $limitedCoin
+                );
+                $useCoinModel->getQueryBuilder($userId)
+                    ->where(UserCoins::USER_ID, '=', $userId)
+                    ->update($userCoinResource);
+
+                // コイン履歴の更新
                 $model->getQueryBuilder($row[UserCoinHistories::USER_ID])->insert($row);
             }
             DB::commit();
@@ -149,5 +195,28 @@ class UserCoinHistoriesTableSeeder extends BaseSeeder
             DB::rollBack();
             throw $e;
         }
+    }
+
+    /**
+     * get user coins by user id.
+     *
+     * @param int $userId user id
+     * @param bool $isLock exec lock For Update
+     * @return array|null
+     */
+    private function getUserCoinByUserId(int $userId, bool $isLock = false): array|null
+    {
+        $userCoin = (new UserCoins())
+            ->getQueryBuilder($userId)
+            ->where(UserCoins::USER_ID, '=', $userId)
+            ->get();
+
+        if ($userCoin->count() === 0) {
+            return null;
+        }
+
+        // 複数チェックはrepository側で実施済み
+        return ArrayLibrary::toArray(ArrayLibrary::getFirst($userCoin->toArray()));
+        ;
     }
 }
