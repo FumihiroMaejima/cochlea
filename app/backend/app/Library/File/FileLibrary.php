12a55a67d3965bea65ec52abe4e666c8f81909e0
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-12-13 01:26:51 +0900

    modify filesystem setting.

--- app/backend/app/Library/File/FileLibrary.php
+++ app/backend/app/Library/File/FileLibrary.php
@@ -3,6 +3,9 @@
 namespace App\Library\File;
 
 use Illuminate\Support\Facades\Storage;
+use App\Exceptions\MyApplicationHttpException;
+use App\Library\Message\StatusCodeMessages;
+use Exception;
 
 class FileLibrary
 {
@@ -18,9 +21,9 @@ class FileLibrary
     public static function getStorageDiskByEnv(): string
     {
         if ((config('app.env') === 'local') || config('app.env') === 'testing') {
-            return self::STORAGE_DISK_LOCAL;
+            return config('filesystems.default') ?? self::STORAGE_DISK_LOCAL;
         } else {
-            return self::STORAGE_DISK_S3;
+            return config('filesystems.default') ?? self::STORAGE_DISK_S3;
         }
     }
 
@@ -29,6 +32,7 @@ class FileLibrary
      *
      * @param string $path file path
      * @return string|null
+     * @throws Exception
      */
     public static function getFileStoream(string $path): string|null
     {
@@ -36,10 +40,20 @@ class FileLibrary
         $file = Storage::disk(self::STORAGE_DISK_LOCAL)->get($path);
 
         // production向けなどS3から取得する時の設定
-        if (!((config('app.env') === 'local') || config('app.env') === 'testing')) {
+        if (
+            !((config('app.env') === 'local') || config('app.env') === 'testing') &&
+            (config('filesystems.default') === self::STORAGE_DISK_S3)
+        ) {
             if (is_null($file)) {
                 // productionの時はenvでデフォルトのストレージを変更するのが適切
                 $file = Storage::disk(self::STORAGE_DISK_S3)->get($path);
+
+                // 保存されていない場合
+                if (is_null($file)) {
+                    throw new MyApplicationHttpException(
+                        StatusCodeMessages::STATUS_404,
+                    );
+                }
                 // ファイルデータそのものを別途レスポンスに返す時はローカルに保存する
                 Storage::disk(self::STORAGE_DISK_LOCAL)->put($path, $file, self::STORAGE_DISK_LOCAL);
             }
