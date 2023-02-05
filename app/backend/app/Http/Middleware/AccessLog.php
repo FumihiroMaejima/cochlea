a52cc172f6b3ecbf906995c90251f6ac2b971119
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-02-05 23:14:46 +0900

    add unit.

--- app/backend/app/Http/Middleware/AccessLog.php
+++ app/backend/app/Http/Middleware/AccessLog.php
@@ -169,8 +169,8 @@ class AccessLog
             'request_content'  => $this->requestContent,
             'plathome'         => $this->plathome,
             'process_id'       => $this->pid,
-            'memory'           => $this->memory,
-            'peak_memory'      => $this->peakMemory,
+            'memory'           => $this->memory . 'Byte',
+            'peak_memory'      => $this->peakMemory . 'Byte',
         ];
 
         // Log::debug($request->method(), ['url' => $request->fullUrl(), 'request' => $request->all()]);
