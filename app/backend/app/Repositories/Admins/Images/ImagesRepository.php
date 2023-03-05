689888fee6ceb95d7a708a4a0e7e6af2491be908
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-03-05 23:35:48 +0900

    modify methods sort.

--- app/backend/app/Repositories/Admins/Images/ImagesRepository.php
+++ app/backend/app/Repositories/Admins/Images/ImagesRepository.php
@@ -63,6 +63,40 @@ class ImagesRepository implements ImagesRepositoryInterface
     }
 
     /**
+     * get by ids.
+     *
+     * @param array $ids rocord ids
+     * @param bool $isLock exec lock For Update
+     * @return Collection|null
+     * @throws MyApplicationHttpException
+     */
+    public function getByIds(array $ids, bool $isLock = false): Collection|null
+    {
+        $collection = DB::table($this->getTable())
+            ->select(['*'])
+            ->whereIn(Images::ID, $ids)
+            ->where(Images::DELETED_AT, '=', null)
+            ->get();
+
+        // 存在しない場合
+        if ($collection->count() === self::NO_DATA_COUNT) {
+            return null;
+        }
+
+        if ($isLock) {
+            // ロックをかけた状態で再検索
+            $collection = DB::table($this->getTable())
+            ->lockForUpdate()
+            ->select(['*'])
+            ->whereIn(Images::ID, $ids)
+            ->where(Images::DELETED_AT, '=', null)
+            ->get();
+        }
+
+        return $collection;
+    }
+
+    /**
      * get Image by uuid.
      *
      * @param string $uuid
@@ -99,40 +133,6 @@ class ImagesRepository implements ImagesRepositoryInterface
                 ->where(Images::DELETED_AT, '=', null)
                 ->lockForUpdate()
                 ->get();
-        }
-
-        return $collection;
-    }
-
-    /**
-     * get by ids.
-     *
-     * @param array $ids rocord ids
-     * @param bool $isLock exec lock For Update
-     * @return Collection|null
-     * @throws MyApplicationHttpException
-     */
-    public function getByIds(array $ids, bool $isLock = false): Collection|null
-    {
-        $collection = DB::table($this->getTable())
-            ->select(['*'])
-            ->whereIn(Images::ID, $ids)
-            ->where(Images::DELETED_AT, '=', null)
-            ->get();
-
-        // 存在しない場合
-        if ($collection->count() === self::NO_DATA_COUNT) {
-            return null;
-        }
-
-        if ($isLock) {
-            // ロックをかけた状態で再検索
-            $collection = DB::table($this->getTable())
-            ->lockForUpdate()
-            ->select(['*'])
-            ->whereIn(Images::ID, $ids)
-            ->where(Images::DELETED_AT, '=', null)
-            ->get();
         }
 
         return $collection;
