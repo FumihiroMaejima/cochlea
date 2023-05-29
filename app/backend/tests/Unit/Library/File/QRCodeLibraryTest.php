e901ea8f0662d45f8cee85dc495452ed66de033d
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-05-30 00:16:22 +0900

    create QRCodeLibraryTest.

---
+++ app/backend/tests/Unit/Library/File/QRCodeLibraryTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit\Library\File;
+
+// use PHPUnit\Framework\TestCase;
+
+use App\Library\File\QRCodeLibrary;
+use Tests\TestCase;
+
+class QRCodeLibraryTest extends TestCase
+{
+    /**
+     * setUpは各テストメソッドが実行される前に実行する
+     * 親クラスのsetUpを必ず実行する
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * test get QRCode data
+     * @return array
+     */
+    public function getQRCodeByUrlDataProvider(): array
+    {
+        $this->createApplication();
+
+        $url = 'http://localhost/test';
+
+        return [
+            'param url, expect html' => [
+                'value' => $url,
+            ],
+        ];
+    }
+
+    /**
+     * test get qr code by url.
+     *
+     * @dataProvider getQRCodeByUrlDataProvider
+     * @param string $value
+     * @return void
+     */
+    public function testgetQRCodeByUrl(string $value): void
+    {
+        $this->assertIsString(QRCodeLibrary::getQrCodeByUrl($value));
+    }
+}
