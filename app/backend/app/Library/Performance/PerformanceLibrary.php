77328b0f8456e07ad02d9edbd560d4b76f177700
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-06-28 00:38:31 +0900

    remove unused.

--- app/backend/app/Library/Performance/PerformanceLibrary.php
+++ app/backend/app/Library/Performance/PerformanceLibrary.php
@@ -4,16 +4,6 @@ namespace App\Library\Performance;
 
 class PerformanceLibrary
 {
-    // メモリ使用量の例
-    // App\Library\Performance\MemoryLibrary::getIntValueListUsage(100); // "8.05 KB"
-    // App\Library\Performance\MemoryLibrary::getIntValueListUsage(1000); // "36.05 KB"
-    // App\Library\Performance\MemoryLibrary::getIntValueListUsage(10000); // "516.05 KB"
-    // App\Library\Performance\MemoryLibrary::getIntValueListUsage(100000); // "4 MB"
-    // App\Library\Performance\MemoryLibrary::getIntValueListUsage(1000000); // "32 MB"
-    // App\Library\Performance\MemoryLibrary::getIntValueListUsage(10000000); // "512 MB"
-
-    public const MEMORY_UNIT = ['B','KB','MB','GB','TB','PB'];
-    public const BASE_BIT = 1024;
 
     public const ONE_DAY_HOURS = 24;
     public const ONE_HOUR_SECONDS = 3600;
