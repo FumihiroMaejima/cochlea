0abc160133f5e1a9d15f79e08d7e9fc0338bf024
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-08-05 23:05:07 +0900

    create service terms export files.

---
+++ app/backend/app/Imports/Masters/ServiceTerms/ServiceTermsImport.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Imports\Masters\ServiceTerms;
+
+use App\Models\Masters\Events;
+use Maatwebsite\Excel\Concerns\ToArray;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\Importable;
+use Illuminate\Http\UploadedFile;
+
+class ServiceTermsImportImport implements ToModel
+{
+    use Importable;
+    private array $resource;
+
+    /**
+     * create constructer.
+     * @param \Illuminate\Http\UploadedFile $file
+     * @return void
+     */
+    public function __construct(UploadedFile $file)
+    {
+        $this->resource = $this->toArray($file, null, \Maatwebsite\Excel\Excel::CSV);
+    }
+
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return $this->resource;
+        /* return new Coins([
+            //
+        ]); */
+    }
+}
---
+++ app/backend/app/Services/Admins/ServiceTermsService.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Services\Admins;
+
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Collection;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use App\Exceptions\MyApplicationHttpException;
+use App\Library\Message\StatusCodeMessages;
+use App\Http\Resources\Admins\HomeContentsGroupsResource;
+use App\Http\Resources\Admins\HomeContentsResource;
+use App\Repositories\Masters\HomeContents\HomeContentsGroupsRepositoryInterface;
+use App\Repositories\Masters\HomeContents\HomeContentsRepositoryInterface;
+use App\Exports\Masters\HomeContents\HomeContentsBulkInsertTemplateExport;
+use App\Exports\Masters\HomeContents\HomeContentsExport;
+use App\Exports\Masters\HomeContents\HomeContentsGroupsBulkInsertTemplateExport;
+use App\Exports\Masters\HomeContents\HomeContentsGroupsExport;
+use App\Imports\Masters\HomeContents\HomeContentsGroupsImport;
+use App\Imports\Masters\HomeContents\HomeContentsImport;
+use App\Library\Array\ArrayLibrary;
+use App\Library\Cache\CacheLibrary;
+use App\Library\Time\TimeLibrary;
+use App\Models\Masters\HomeContentsGroups;
+use App\Models\Masters\HomeContents;
+use Exception;
+
+class ServiceTermsService
+{
+    // cache keys
+    private const CACHE_KEY_HOME_CONTENTS_COLLECTION_LIST = 'admin_home_contents_collection_list';
+
+    protected HomeContentsGroupsRepositoryInterface $homeContentsGroupsRepository;
+    protected HomeContentsRepositoryInterface $homeContentsRepository;
+
+    /**
+     * create service instance
+     *
+     * @param HomeContentsGroupsRepositoryInterface $homeContentsGroupsRepository
+     * @param HomeContentsRepositoryInterface $homeContentsRepository
+     * @return void
+     */
+    public function __construct(
+        HomeContentsGroupsRepositoryInterface $homeContentsGroupsRepository,
+        HomeContentsRepositoryInterface $homeContentsRepository,
+    ) {
+        $this->homeContentsGroupsRepository = $homeContentsGroupsRepository;
+        $this->homeContentsRepository = $homeContentsRepository;
+    }
+
+    /**
+     * download home contents data service
+     *
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
+    public function downloadCSVForHomeContents()
+    {
+        $data = $this->homeContentsRepository->getRecords();
+
+        return Excel::download(new HomeContentsExport($data), 'home_contents_info_' . TimeLibrary::getCurrentDateTime(TimeLibrary::DATE_TIME_FORMAT_YMDHIS) . '.csv');
+    }
+
+    /**
+     * download home contents template data service
+     *
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
+    public function downloadTemplateForHomeContents()
+    {
+        return Excel::download(
+            new HomeContentsBulkInsertTemplateExport(collect(Config::get('myappFile.service.admins.homeContents.template'))),
+            'master_home_contents_template_' . TimeLibrary::getCurrentDateTime(TimeLibrary::DATE_TIME_FORMAT_YMDHIS) . '.csv'
+        );
+    }
+
+
+    /**
+     * imort home contents by template data service
+     *
+     * @param UploadedFile $file
+     * @return JsonResponse
+     */
+    public function importTemplateForHomeContents(UploadedFile $file)
+    {
+        // ファイル名チェック
+        if (!preg_match('/^master_home_contents_template_\d{14}\.csv/u', $file->getClientOriginalName())) {
+            throw new MyApplicationHttpException(
+                StatusCodeMessages::STATUS_422,
+                'no include title.'
+            );
+        }
+
+        DB::beginTransaction();
+        try {
+            $fileData = Excel::toArray(new HomeContentsImport($file), $file, null, \Maatwebsite\Excel\Excel::CSV);
+
+            $resource = HomeContentsResource::toArrayForBulkInsert(current($fileData));
+
+            $insertCount = $this->homeContentsRepository->create($resource);
+
+            DB::commit();
+
+            // キャッシュの削除
+            // CacheLibrary::deleteCache(self::CACHE_KEY_HOME_CONTENTS_COLLECTION_LIST, true);
+
+            // レスポンスの制御
+            $message = ($insertCount > 0) ? 'success' : 'Bad Request';
+            $status = ($insertCount > 0) ? 201 : 401;
+
+            return response()->json(['message' => $message, 'status' => $status], $status);
+        } catch (Exception $e) {
+            Log::error(__CLASS__ . '::' . __FUNCTION__ . ' line:' . __LINE__ . ' ' . 'message: ' . json_encode($e->getMessage()));
+            DB::rollback();
+            throw new MyApplicationHttpException(
+                StatusCodeMessages::STATUS_500,
+                $e->getMessage(),
+                previous: $e
+            );
+        }
+    }
+}
