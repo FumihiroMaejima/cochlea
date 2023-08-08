a475cdce3ffc136bba1bc07402055ba123fdb3fa
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-08-09 01:01:08 +0900

    add csv option.

--- scripts/get-dump-example.sh
+++ scripts/get-dump-example.sh
@@ -13,6 +13,8 @@ DATABASE_USER=database_user
 DATABASE_PASSWORD=database_password
 DATABASE_NAME=database_name
 OUTPUT_FILE=sample/dump/dump_${TIME_STAMP}.sql
+SECURE_FILE_PRIV_DIR=/var/lib/mysql-files
+OUTPUT_CSV_FILE=scripts/dump_${TIME_STAMP}.csv
 
 # @param {string} message
 showMessage() {
@@ -27,6 +29,9 @@ showMessage ${START_MESSAGE}
 if [ "$1" != '' ]; then
   if [ "$1" == 'gz' ]; then
     docker exec -it ${DATABASE_CONTAINER_NAME} mysqldump -u ${DATABASE_USER} -p${DATABASE_PASSWORD} ${DATABASE_NAME} | gzip > ${OUTPUT_FILE}.gz
+  elif [ "$1" == 'csv' ]; then
+    # only ouput in docker container
+    docker exec -it ${DATABASE_CONTAINER_NAME} mysqldump -u ${DATABASE_USER} -p${DATABASE_PASSWORD} --tab=${SECURE_FILE_PRIV_DIR} --fields-terminated-by=, ${DATABASE_NAME}
   fi
 else
   # dump command.
