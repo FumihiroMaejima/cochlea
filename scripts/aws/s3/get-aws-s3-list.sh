e92f68757c804e3ffaf9b8ecb38016b9a6af8ef7
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-08-11 22:31:25 +0900

    create get aws-s3-list batch.

---
+++ scripts/aws/s3/get-aws-s3-list.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# CURRENT_DIR=$(cd $(dirname $0); pwd)
+DELIMITER_LINE='------------------------------------------------------'
+START_MESSAGE='start Get S3 List'
+
+AWS_CLI_PATH=/usr/local/bin/aws
+
+
+# @param {string} message
+showMessage() {
+  echo ${DELIMITER_LINE}
+  echo $1
+}
+
+# process start
+showMessage "$START_MESSAGE"
+
+# iam users
+$AWS_CLI_PATH s3 ls
+
+showMessage "Get S3 Users"
+
