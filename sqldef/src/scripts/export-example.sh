56acd01e2e4043bc44c06780af96ef9130e63851
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-09-06 00:36:14 +0900

    add output path.

--- sqldef/src/scripts/export-example.sh
+++ sqldef/src/scripts/export-example.sh
@@ -15,7 +15,8 @@ DATABASE_PORT=3306
 DATABASE_USER=database_user
 DATABASE_PASSWORD=database_password
 DATABASE_NAME=database_name
-OUTPUT_FILE=sqldef/src/sqls/dump_${TIME_STAMP}.sql # 存在するディレクトリである必要がある(scripts/databaseなど)
+OUTPUT_PATH=sqldef/src/sqls/
+OUTPUT_FILE=dump_${TIME_STAMP}.sql # 存在するディレクトリである必要がある(scripts/databaseなど)
 SECURE_FILE_PRIV_DIR=/var/lib/mysql-files
 OUTPUT_CSV_FILE=scripts/database/dump_${TIME_STAMP}.csv
 
@@ -30,10 +31,10 @@ showMessage ${START_MESSAGE}
 
 # parameter check
 if [ "$1" != '' ]; then
-  docker-compose exec ${SERVICE_NAME} ${SERVICE_BIN_PATH} -h ${DATABASE_HOST} -P ${DATABASE_PORT} -u ${DATABASE_USER} -p ${DATABASE_PASSWORD} $1 --export > ${OUTPUT_FILE}
+  docker-compose exec ${SERVICE_NAME} ${SERVICE_BIN_PATH} -h ${DATABASE_HOST} -P ${DATABASE_PORT} -u ${DATABASE_USER} -p ${DATABASE_PASSWORD} $1 --export > ${OUTPUT_PATH}"$1_"${OUTPUT_FILE}
 else
   # command.
-  docker-compose exec ${SERVICE_NAME} ${SERVICE_BIN_PATH} -h ${DATABASE_HOST} -P ${DATABASE_PORT} -u ${DATABASE_USER} -p ${DATABASE_PASSWORD} ${DATABASE_NAME} --export > ${OUTPUT_FILE}
+  docker-compose exec ${SERVICE_NAME} ${SERVICE_BIN_PATH} -h ${DATABASE_HOST} -P ${DATABASE_PORT} -u ${DATABASE_USER} -p ${DATABASE_PASSWORD} ${DATABASE_NAME} --export > ${OUTPUT_PATH}${DATABASE_NAME}_${OUTPUT_FILE}
 fi
 
 showMessage 'export data base.'
