02eca2a73e7ec8b4f0ea404e8a262680b9b280a0
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-09-07 00:37:00 +0900

    add dry-run option.

--- sqldef/src/scripts/import-example.sh
+++ sqldef/src/scripts/import-example.sh
@@ -18,6 +18,7 @@ DATABASE_NAME=database_name
 OUTPUT_PATH=sqldef/src/sqls/
 OUTPUT_FILE=dump.sql # 存在するディレクトリである必要がある(scripts/databaseなど)
 OUTPUT_CSV_FILE=scripts/database/dump_${TIME_STAMP}.csv
+DRY_RUN_OPTION=--dry-run
 
 # @param {string} message
 showMessage() {
@@ -30,8 +31,12 @@ showMessage ${START_MESSAGE}
 
 # parameter check
 if [ "$1" != '' ]; then
-  # -T is required to avoid error: the input device is not a TTY
-  docker-compose exec -T ${SERVICE_NAME} ${SERVICE_BIN_PATH} -h ${DATABASE_HOST} -P ${DATABASE_PORT} -u ${DATABASE_USER} -p ${DATABASE_PASSWORD} $1 < ${OUTPUT_PATH}${OUTPUT_FILE}
+  if [ "$2" == 'dryRun' ]; then
+    docker-compose exec -T ${SERVICE_NAME} ${SERVICE_BIN_PATH} -h ${DATABASE_HOST} -P ${DATABASE_PORT} -u ${DATABASE_USER} -p ${DATABASE_PASSWORD} $1 ${DRY_RUN_OPTION} < ${OUTPUT_PATH}${OUTPUT_FILE}
+  else
+    # -T is required to avoid error: the input device is not a TTY
+    docker-compose exec -T ${SERVICE_NAME} ${SERVICE_BIN_PATH} -h ${DATABASE_HOST} -P ${DATABASE_PORT} -u ${DATABASE_USER} -p ${DATABASE_PASSWORD} $1 < ${OUTPUT_PATH}${OUTPUT_FILE}
+  fi
 else
   # command.
   docker-compose exec -T ${SERVICE_NAME} ${SERVICE_BIN_PATH} -h ${DATABASE_HOST} -P ${DATABASE_PORT} -u ${DATABASE_USER} -p ${DATABASE_PASSWORD} ${DATABASE_NAME} < ${OUTPUT_PATH}${OUTPUT_FILE}
