16adb34cdcab3bd1075c0fa6f1bd8357e421abf5
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-09-15 02:11:59 +0900

    create testPaging method.

--- app/backend/tests/Unit/Library/Array/ArrayLibraryTest.php
+++ app/backend/tests/Unit/Library/Array/ArrayLibraryTest.php
@@ -58,6 +58,49 @@ class ArrayLibraryTest extends TestCase
     }
 
     /**
+     * array paging data
+     * @return array
+     */
+    public function pagingDataProvider(): array
+    {
+        $this->createApplication();
+
+        $testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        return [
+            'page:0/limit:3' => [
+                'items'  => $testArray,
+                'page'   => 0,
+                'limit'  => 3,
+                'expect' => [1, 2, 3],
+            ],
+            'page:1/limit:3' => [
+                'items'  => $testArray,
+                'page'   => 1,
+                'limit'  => 3,
+                'expect' => [4, 5, 6],
+            ],
+            'page:3/limit:3' => [
+                'items'  => $testArray,
+                'page'   => 3,
+                'limit'  => 3,
+                'expect' => [10],
+            ],
+            'page:4/limit:3' => [
+                'items'  => $testArray,
+                'page'   => 4,
+                'limit'  => 3,
+                'expect' => [],
+            ],
+            'page:0/limit:null' => [
+                'items'  => $testArray,
+                'page'   => 0,
+                'limit'  => null,
+                'expect' => $testArray,
+            ],
+        ];
+    }
+
+    /**
      * test to array.
      *
      * @dataProvider arraySampleDataProvider
@@ -89,5 +132,16 @@ class ArrayLibraryTest extends TestCase
         // echo var_dump($result);
 
         $this->assertEquals(current(json_decode(json_encode($data), true)), ArrayLibrary::getFirst($result));
+    }
+
+    /**
+     * test paging array items.
+     *
+     * @dataProvider pagingDataProvider
+     * @return void
+     */
+    public function testPaging(array $items, int $page, ?int $limit, array $expect): void
+    {
+        $this->assertEquals($expect, ArrayLibrary::paging($items, $page, $limit));
     }
 }
