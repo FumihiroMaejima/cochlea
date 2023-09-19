db78df04cd179d9eb4c04a94da6acd46f67c16d1
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-09-20 00:04:37 +0900

    create createApplicationForStaticDataProvider.

--- app/backend/tests/CreatesApplication.php
+++ app/backend/tests/CreatesApplication.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
 
 trait CreatesApplication
 {
@@ -12,6 +13,20 @@ trait CreatesApplication
      * @return \Illuminate\Foundation\Application
      */
     public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+    /**
+     * Creates the application for static data provider.
+     *
+     * @return Application
+     */
+    public function createApplicationForStaticDataProvider(): Application
     {
         $app = require __DIR__.'/../bootstrap/app.php';
 
