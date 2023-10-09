c50e68aaccc32fa905d796fb58fed70384cc4157
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-10-09 19:24:21 +0900

    use const.

--- scripts/docker/redis-cluster-dev.sh
+++ scripts/docker/redis-cluster-dev.sh
@@ -8,7 +8,6 @@ DOCKER_NETWORK_NAME='cochlea-net'
 DOCKER_SERVICE_NAME='redis-cluster'
 DOCKER_REDIS_PORT=6379
 REDIS_CLUSTER_REPLICA_COUNT=1
-GET_CURRENT_NETWORK_STATUS_COMMAND="docker network inspect cochlea-net | jq '.[0].Containers | .[].IPv4Address'"
 
 # @param {string} message
 showMessage() {
@@ -31,10 +30,10 @@ fi
 if [[ "$(docker-compose -f ${DOCKER_COMPOSE_FILE} ps -q 2>/dev/null)" == "" ]]; then
   # コンテナが立ち上がっていない状態の時
   showMessage 'Up Docker Container!'
-  docker-compose -f ${DOCKER_COMPOSE_FILE} up -d --scale redis-cluster=$1
+  docker-compose -f ${DOCKER_COMPOSE_FILE} up -d --scale ${DOCKER_SERVICE_NAME}=$1
 
   # dockerに割り当てられたIPの取得(jqコマンドを利用)
-  TARGET_IPS=`docker network inspect cochlea-net | jq '.[0].Containers | .[].IPv4Address'`;
+  TARGET_IPS=`docker network inspect ${DOCKER_NETWORK_NAME} | jq '.[0].Containers | .[].IPv4Address'`;
   # cluster設定を行うIPとportの組み合わせ
   CLUSTER_IPS_STRING=''
   for TARGET_IP in ${TARGET_IPS[@]}
@@ -42,7 +41,7 @@ if [[ "$(docker-compose -f ${DOCKER_COMPOSE_FILE} ps -q 2>/dev/null)" == "" ]]; 
     # 各IPアドレスの取得(「"」とは不要の為削除)
     TMP_IP="${TARGET_IP//\"/}"
     # ネットワーク部指定の為のCIDR表記のbit指定も削除
-    echo "${TMP_IP//\/16/}:${DOCKER_REDIS_PORT}"
+    echo "Target IP is: ${TMP_IP//\/16/}:${DOCKER_REDIS_PORT}"
     # 文字列連結
     CLUSTER_IPS_STRING="${CLUSTER_IPS_STRING} ${TMP_IP//\/16/}:${DOCKER_REDIS_PORT}"
   done
@@ -50,14 +49,14 @@ if [[ "$(docker-compose -f ${DOCKER_COMPOSE_FILE} ps -q 2>/dev/null)" == "" ]]; 
 
   # redis clusterの作成(対話形式の箇所がある為パラメーターを渡す)
   # docker compose exec redis-cluster redis-cli --cluster create ${CLUSTER_IPS_STRING} --cluster-replicas ${REDIS_CLUSTER_REPLICA_COUNT}
-  docker compose exec redis-cluster ash -c "echo yes | redis-cli --cluster create ${CLUSTER_IPS_STRING} --cluster-replicas ${REDIS_CLUSTER_REPLICA_COUNT}"
+  docker compose exec ${DOCKER_SERVICE_NAME} ash -c "echo yes | redis-cli --cluster create ${CLUSTER_IPS_STRING} --cluster-replicas ${REDIS_CLUSTER_REPLICA_COUNT}"
 
   # clusterの設定確認
   echo ${DELIMITER_LINE}
-  docker-compose exec redis-cluster redis-cli cluster nodes
+  docker-compose exec ${DOCKER_SERVICE_NAME} redis-cli cluster nodes
   # networkの設定確認
   echo ${DELIMITER_LINE}
-  docker network inspect cochlea-net | jq '.[0].Containers | .[] | {Name, IPv4Address}'
+  docker network inspect ${DOCKER_NETWORK_NAME} | jq '.[0].Containers | .[] | {Name, IPv4Address}'
 else
   # コンテナが立ち上がっている状態の時
   showMessage 'Down Docker Container!'
