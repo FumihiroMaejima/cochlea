4562d0792aed73f34d63d37269f9d31f68138d88
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-11-07 00:04:34 +0900

    add declare strict_type to batch.

--- app/backend/app/Console/Commands/Admins/Coin/UserCoinHistoriesCountingCommand.php
+++ app/backend/app/Console/Commands/Admins/Coin/UserCoinHistoriesCountingCommand.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Console\Commands\Admins\Coin;
 
 use Illuminate\Console\Command;
--- app/backend/app/Console/Commands/Debug/Seeder/UserCoinHistoriesExpiredSeedCommand.php
+++ app/backend/app/Console/Commands/Debug/Seeder/UserCoinHistoriesExpiredSeedCommand.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Console\Commands\Debug\Seeder;
 
 use Illuminate\Console\Command;
