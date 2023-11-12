4c9ef110edc3ce1b02ce673ac6bcebeb8632f35f
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-11-12 19:59:45 +0900

    add strict_type in file, encrypt.

--- app/backend/app/Library/Debug/WebConsole.php
+++ app/backend/app/Library/Debug/WebConsole.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Library\Debug;
 
 use App\Exceptions\MyApplicationHttpException;
--- app/backend/app/Library/File/FileLibrary.php
+++ app/backend/app/Library/File/FileLibrary.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Library\File;
 
 use Illuminate\Support\Facades\Storage;
