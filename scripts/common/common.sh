860042779ee7c6173d3724e0d964d23b9f7a0b8c
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-12-19 23:58:08 +0900

    add validation check.

--- scripts/common/common.sh
+++ scripts/common/common.sh
@@ -6,3 +6,20 @@ function showMessage() {
   echo $1
 }
 
+# {unknown} parameter
+function isParamterNotEmpty() {
+if [ "$1" == '' ]; then
+  showMessage 'Invalid Parameter. \nNo parameter.'
+  exit
+fi
+}
+
+# {string} parameter
+# {string} target
+function isContainTargetString() {
+if [[ "$1" != *$2* ]]; then
+  showMessage "Invalid Parameter. \nNo contains '$2'."
+  exit
+fi
+}
+
