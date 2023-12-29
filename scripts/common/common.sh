6cc5a7d58beeed528cbad23d6bb4b4309fb6caf7
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-12-30 01:37:41 +0900

    add getArrayElement.

--- scripts/common/common.sh
+++ scripts/common/common.sh
@@ -23,3 +23,23 @@ if [[ "$1" != *$2* ]]; then
 fi
 }
 
+# @param {string} value of array
+# @return {string} value
+function getArrayElement() {
+  local TMP="$1"
+  if [ "$TMP" == '[' ]; then
+    # 実行結果は空文字列
+    continue
+  elif [ "$TMP" == ']' ]; then
+    continue
+  elif [ "$TMP" == '[]' ]; then
+    continue
+  else
+    # "を削除
+    local TMP_ID="${TMP//\"/}"
+    # ,を削除
+    TMP_ID="${TMP_ID//,/}"
+    echo $TMP_ID
+  fi
+}
+
