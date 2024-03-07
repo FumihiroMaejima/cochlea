778efe9bd80f436507a3d0c1b0b941e9da08f5f9
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-03-07 23:57:31 +0900

    add private const to trait.

--- app/backend/app/Trait/HelperTrait.php
+++ app/backend/app/Trait/HelperTrait.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Config;
 
 trait HelperTrait
 {
+    private const ENV_PRD = 'production';
+
     /**
      * get url by route name
      * @description now route helper is bagged in unit test setup(). (return double host ex: http://localhost/localhost/)
@@ -19,6 +21,6 @@ trait HelperTrait
     public function getRouteUrl(string $name, array $parameters = [])
     {
         // return "http://".Config::get('app.url').route($name, $parameters, false);
-        return (Config::get('app.env') === 'production') ? "https://" : "http://" .Config::get('app.url').route($name, $parameters, false);
+        return (Config::get('app.env') === self::ENV_PRD) ? "https://" : "http://" .Config::get('app.url').route($name, $parameters, false);
     }
 }
