8ae6b1dba5cc0ca24c9a944ccc243676cdad3833
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-06-22 00:26:06 +0900

    create ProcessLibrary.

---
+++ app/backend/app/Library/Process/ProcessLibrary.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Library\Process;
+
+use stdClass;
+use App\Exceptions\MyApplicationHttpException;
+use App\Library\Message\StatusCodeMessages;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class ProcessLibrary
+{
+    /**
+     * sampel process.
+     * @return string
+     */
+    public static function sampleProcess(): string
+    {
+        $process = new Process(['ls', 'app', '-a']);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        return $process->getOutput();
+    }
+}
