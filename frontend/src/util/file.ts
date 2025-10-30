69b3d5252a506dac039608311d7b99d0139c730d
Fumihiro_Maejima <35716669+FumihiroMaejima@users.noreply.github.com>
2025-10-31 01:02:55 +0900

    add base64 data image, size setting.

--- frontend/src/util/file.ts
+++ frontend/src/util/file.ts
@@ -31,3 +31,44 @@ export const createFileRequestFormData = (
   data.append(key, file)
   return data
 }
+
+/**
+ * 画像の縦横サイズの取得(データURL版)
+ * @param {string} dataUrl base64などのDataURL形式の画像データ
+ * @return {Promise<number[]>}
+ */
+export const getImageWidthAndHeightByDataUrl = async (dataUrl: string): Promise<number[]> => {
+  const image = new Image()
+  return new Promise(
+    (resolve: (param: number[]) => void, reject: (reason: unknown) => void) => {
+      image.onload = () => {
+        const width = image.naturalWidth
+        const height = image.naturalHeight
+        // 読み込んだ結果をresolve(解決)する
+        resolve([width, height])
+      }
+
+      image.onerror = (error) => {
+        // throw new Error('get width, height error: ' + (typeof error === 'string') ? error : '')
+        reject(error)
+      }
+
+      // 読み込み
+      image.src = dataUrl
+    },
+  )
+}
+
+/**
+ * base64エンコードデータのサイズ取得
+ * @param {string} base64Data base64エンコードデータ
+ * @return {Promise<number[]>}
+ */
+export const getBase64FileSize = (base64Data: string): number => {
+  // データURLの「,」以降を取得(「data:image/png;base64,」などのprefix部分を除去)
+  const cleaned = base64Data.split(',').pop() ?? ''
+  // パディングの数を取得(末尾の「=」の数)
+  const padding = (cleaned.match(/=+$/)?.[0].length ?? 0)
+  // サイズ計算(元データのバイト数を計算。base64は元データの3バイト4文字にエンコードするため、その比率で計算)
+  return Math.floor((cleaned.length * 3) / 4) - padding
+}
