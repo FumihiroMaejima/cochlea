21030f16e0f9fed973283abba111a028521c41e4
Fumihiro_Maejima <35716669+FumihiroMaejima@users.noreply.github.com>
2025-11-19 01:33:54 +0900

    modify controller setting.

--- app/backend/app/Http/Controllers/Controller.php
+++ app/backend/app/Http/Controllers/Controller.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+/* abstract class Controller
+{
+    //
+} */
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
